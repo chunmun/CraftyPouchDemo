cb4ea4adcc9dc8d036944aaa4ffe248a6c568402
chunmun <xilasirc@gmail.com>
2013-03-26 16:34:04 +0800

    Add syncing to external database

--- src/game.js
+++ src/game.js
@@ -2,8 +2,9 @@ var Game = (function (Game) {
   var Game = {
     start : function() {
       Crafty.init(Game.config.canvasWidth, Game.config.canvasHeight);
+      // Couchdb database must be already created before connection
+      Crafty.storage.external('http://localhost:2020/gamedb');
       Crafty.storage.open('Game');
-      Crafty.storage.external('localhost:2020');
       Crafty.background('rgb(255,255,255)');
       Crafty.scene('Loading');
     },
